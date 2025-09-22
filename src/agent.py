3c97e5d1e3341a4562b787a11fdb080f2772afeb
BennyTaccardi <benny.taccardi@gmail.com>
2025-09-23 00:10:01 +0200

    chroma httpclient

--- src/agent.py
+++ src/agent.py
@@ -5,20 +5,6 @@ from pdf_loader import load_papers, split_text
 if '__main__' == __name__:
 
     paper_collection = PaperCollection()
-    paper_collection.add([
-        Paper(
-            text="Artificial intelligence is transforming industries across the globe.",
-            id="title-1"
-            ),
-        Paper(
-            text="Vector databases enable efficient semantic search capabilities.",
-            id="title-2"
-            ),
-        ])
-    # results = paper_collection.query(
-    #     query_texts=["How is AI changing businesses?"],
-    #     n_results=1
-    # )
 
     pdfs = load_papers()
     for pdf in pdfs:
@@ -30,8 +16,8 @@ if '__main__' == __name__:
         ])
         
     results = paper_collection.query(
-        query_texts=["In a self-attention layer all of the keys"],
+        query_texts=["WHat is the auto-prompt technique"],
         n_results=10
     )
     
-    print(results)
+    print(results.get("ids", None))
--- src/clients.py
+++ src/clients.py
@@ -1,3 +1,4 @@
 import chromadb
 
-chroma_client = chromadb.Client()
+chroma_client = chromadb.HttpClient(host='localhost', port=8000)
+chroma_client.heartbeat()
